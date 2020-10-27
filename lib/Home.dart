c68ccf53bc2d60aeca48912cb727638fa3f6b99b
adityapuri75 <adityapuri75@gmail.com>
2020-10-28 00:33:08 +0530

    Ui Phase 1

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,28 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:puri_fast_food/widgets/Home_Top_Info.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
-final textstyle = TextStyle(fontSize: 32.0,fontWeight: FontWeight.bold);
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Text("What Would",style: textstyle,),
-                  Text("you like to eat?",style: textstyle,),
-                ],
-              )
-            ],
-
-          ),
-        ],
+      backgroundColor: Colors.yellow,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+        child: ListView(
+          children: <Widget>[
+            Home_Top_Info(),
+          ],
+        ),
       ),
     );
   }
